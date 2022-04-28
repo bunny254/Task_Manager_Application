8be03927738d877e6a7b3c4e637d838cec2c7849
bunny254 <kimaniwachira03@gmail.com>
2022-04-28 15:15:55 +0300

    connected to DB and created Task Schema

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const express=require('express')
+const express=require('express');
+const connectDB = require('./db/connectDb');
 const app=express()
 const port=3500;
 const tasks=require('./routes/tasks')
+require('dotenv').config();
 
 //middleware
 app.use(express.json())
@@ -19,6 +21,15 @@ app.use('/api/v1/tasks', tasks)
 //app.patch('/api/vi/tasks')    -update a task
 //app.delete('/api/vi/tasks')   -delete a task
 
-app.listen(port,()=>{
-    console.log(`App is running on ${port}...`)
-})
+const start= async()=>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,()=>{
+            console.log(`App is running on ${port}...`)
+        })
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+start ()
