8673849ee9bf0db1b44a456f55cb96fcae689fa3
bunny254 <kimaniwachira03@gmail.com>
2022-04-29 08:28:10 +0300

    Create task, get all tasks, get single task with Id

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,35 @@
-const getAllTasks= (req,res)=>{
-    res.send("Get all tasks")
+const Task= require('../models/task');
+const getAllTasks= async (req,res)=>{
+    try {
+        const tasks= await Task.find({})
+        res.status(200).json({tasks})
+    } catch (error) {
+        res.status(500).json({msg:error})
+        
+    }
+    //res.send("Get all tasks")
 }
 
-const createTask= (req,res)=>{
-    res.json(req.body)
+const createTask= async (req,res)=>{
+    try {
+    const task= await Task.insertMany(req.body)
+    res.status(201).json({task}) 
+    } catch (error) {
+        res.status(500).json({msg:error})        
+    }
 }
-
-const getTask= (req,res)=>{
-    res.json({id:req.params.id})
+const getTask= async (req,res)=>{
+    try {
+       const {id:taskID}= req.params
+        const singleTask= await Task.findOne({_id: taskID})
+        if (!singleTask){
+            return res.status(404).json({msg: `No such task id: ${taskID}`})
+        }
+        res.status(200).json({singleTask})        
+    } catch (error) {
+        res.status(500).json({msg:error})        
+    }
+    //res.json({id:req.params.id})
 }
 const updateTask= (req,res)=>{
     res.send('Update task')
